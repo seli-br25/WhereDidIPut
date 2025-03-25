c5dea57a58374aa33e4895b9073b8a1237d45d1c
Selina Breuer <selina.breuer@tuwien.ac.at>
2025-03-25 14:16:35 +0100

    finished trash task

--- Assets/Scripts/TrashMechanic.cs
+++ Assets/Scripts/TrashMechanic.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class TrashMechanic : MonoBehaviour
 {
+    private int trashCount = 0;
+    public bool taskDone = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Trash"))
         {
-            Debug.Log("Trash collected!");
+            trashCount++;
+            Debug.Log("Trash entered: " + trashCount);
+            if (trashCount >= 4)
+            {
+                taskDone = true;
+                Debug.Log("Task complete!");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Trash"))
+        {
+            trashCount--;
+            Debug.Log("Trash exited: " + trashCount);
+            if (trashCount < 4)
+            {
+                taskDone = false;
+            }
         }
     }
 }
